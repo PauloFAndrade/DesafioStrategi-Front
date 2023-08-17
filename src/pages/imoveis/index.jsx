d36a4367338690471d2e46d53615efeb42b91601
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2023-08-17 11:44:41 -0300

    Update index.jsx

--- src/pages/imoveis/index.jsx
+++ src/pages/imoveis/index.jsx
@@ -59,7 +59,7 @@ function Imoveis() {
         async function getImoveis() {
 
             try {
-                const response = await fetch('http://localhost:3000/imoveis', {
+                const response = await fetch('http://localhost:3001/imoveis', {
                     method: "GET",
                     headers: {
                         Authorization: `Bearer ${token}`
